94a79cdcb9f001a7751903232ffe4c4587f7a5fe
Benedict Chen <BCHH01.24@ICHAT.SP.EDU.SG>
2025-05-23 19:03:23 +0800

    Redo of BMI file

--- BMI.py
+++ BMI.py
@@ -1,27 +1,26 @@
-def calculate_bmi(weight, height):
-    bmi = weight / (height ** 2)
-    print(f"BMI: {bmi:.2f}")
-
+def calculate_bmi(weight,height,cls):
+    bmi = weight / (height**2)
     if bmi < 18.5:
-        return -1
-    elif 18.5 <= bmi < 25.0:
-        return 0
+        cls =-1
+    elif 18.5<bmi<25:
+        cls = 0
     else:
-        return 1
-
+        cls = 1
+    
+    return bmi , cls
 
 def get_user_input():
-    weight = float(input("Enter your weight (kg): "))
-    height = float(input("Enter your height (m): "))
-    return weight, height
+    weight = float(input("Please input your weight in kg :"))
+    height = float(input("Please input your height in m: "))
+    return weight , height
 
 if __name__ == "__main__":
-    weight, height = get_user_input()
-    classification = calculate_bmi(weight, height)
-    
-    if classification == -1:
-        print("You are underweight.")
-    elif classification == 0:
-        print("You have a normal weight.")
-    else:
-        print("You are overweight.")
+    weight , height = get_user_input()
+    bmi , cls = calculate_bmi(weight , height)
+    print("Your BMI is:" , bmi) 
+    if cls == -1:
+        print("You are underweight")
+    elif cls == 0 :
+        print("Your weight is normal")
+    elif cls == 1:
+        print("You are overweight")
